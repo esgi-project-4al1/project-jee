489ee421eefc997bcde49fa2e4be60766db08b2c
aristide_fumo <fumo.aristide@gmail.com>
2024-05-01 19:49:53 +0200

    first commit

---
+++ rent-property/src/main/java/com/rent/car/rentproperty/RentPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.rent.car.rentproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentPropertyApplication.class, args);
+    }
+
+}
---
+++ rent-property/src/test/java/com/rent/car/rentproperty/RentPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rent.car.rentproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentPropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
